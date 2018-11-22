e44eb525cdb0c6324585a0a055449de303ec54ab
Thomas Ng <D3icidal@gmail.com>
2018-11-21 22:06:07 -0500

    fix home Index and Show pages to use axios

--- src/views/HomesIndex.vue
+++ src/views/HomesIndex.vue
@@ -1,22 +1,40 @@
 <template>
   <!-- INDEX PAGE FOR ALL OF A USERS HOMES -->
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>{{ message }}</h1> <br>
+    <div v-for="home in homes">
+      <br>
+      <p>Name: {{home.name}} </p>
+      <p>Bedrooms: {{home.bedrooms}}  |  
+      Bathrooms: {{home.bathrooms}}<br>
+      State: {{home.state}}</p><br>
+      <br> 
+      <!-- ADD BUTTON TO GO TO SHOW TODO -->
+    </div>
   </div>
+  
 </template>
 
 <style>
 </style>
 
 <script>
-export default {
-  data: function() {
-    return {
-      message: "Homes.vue INDEX PAGE FOR ALL OF A USERS HOMES"
-    };
-  },
-  created: function() {},
-  methods: {},
-  computed: {}
-};
+  // var axios = require('axios');
+  import axios from 'axios'
+  export default {
+    data: function() {
+      return {
+        message: "Homes.vue INDEX PAGE FOR ALL OF A USERS HOMES",
+        homes: [] //#homes array for DOM
+      };
+    },
+    created: function() {
+      axios.get('http://localhost:3000/homes').then(response => {
+        console.log(response.data);
+        this.homes = response.data;
+      });
+    },
+    methods: {},
+    computed: {}
+  };
 </script>
--- src/views/HomesShow.vue
+++ src/views/HomesShow.vue
@@ -1,21 +1,39 @@
 <template>
+  <!-- INDEX PAGE FOR ALL OF A USERS HOMES -->
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>{{ message }}</h1> <br>
+    <div class="homeShow">
+      <br>
+      <p>Name: {{home.name}} </p>
+      <p>Bedrooms: {{home.bedrooms}}  |  
+      Bathrooms: {{home.bathrooms}}<br>
+      State: {{home.state}}</p><br>
+      <br> 
+    </div>
   </div>
+  
 </template>
 
 <style>
 </style>
 
 <script>
-export default {
-  data: function() {
-    return {
-      message: "HomesShow.vue - Show page for a individual home - host view"
-    };
-  },
-  created: function() {},
-  methods: {},
-  computed: {}
-};
+  // var axios = require('axios');
+  import axios from 'axios'
+  export default {
+    data: function() {
+      return {
+        message: "HomesShow.vue Show page for one home - host view",
+        home: {}
+      };
+    },
+    created: function() {
+      axios.get('http://localhost:3000/homes/' + this.$route.params.id).then(response => {
+        console.log(response.data);
+        this.home = response.data;
+      });
+    },
+    methods: {},
+    computed: {}
+  };
 </script>
